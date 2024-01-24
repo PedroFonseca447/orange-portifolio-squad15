7ddecc1e4b6b453a4a32f5ef855d4dd01fcd6714
MayumiNascimento <mayuminascimento9@gmail.com>
2024-01-24 13:27:08 -0300

    feat: integrando menu de navegação e componente Descobrir ao App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import MeusProjetos from './pages/MeusProjetos/MeusProjetos'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Descobrir from './Pages/Descobrir/Descobrir'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<MeusProjetos/>}/>
         <Route path='/meus-projetos' element={<MeusProjetos/>}/>
+        <Route path='/descobrir' element={<Descobrir />}/>
       </Routes>
     </BrowserRouter>
   )
