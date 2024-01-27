7a41e0a55a6242b964fcd56438c905d6b0890474
JM <jaomarcello7@gmail.com>
2024-01-27 16:03:53 -0300

    estilizacao parcial pagina cadastro, tirei o login automatico ao criar usuario

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,117 +3,194 @@ import React, { useState } from "react";
 import { auth } from "../services/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
+import { Typography, Button, InputAdornment, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import TextField from "@mui/material/TextField";
+import imgCadastro from "/Users/macbook/Desktop/projeto-squad15/orange-portifolio-squad15/src/assets/img_cadastro.png";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+
+  const resetForm = () => {
+    setEmail("");
+    setPassword("");
+    setShowPassword(false);
+    setFirstName("");
+    setLastName("");
+  };
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // Firebase signup
+      // Sobe user para o Firebase
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       );
 
-      // Retrieve Firebase user data
+      // Retorno de alguns dados do Firebase
       const user = userCredential.user;
 
-      // Load existing user information or create an empty object
-      const existingUser = JSON.parse(localStorage.getItem("user")) || {};
-
-      // Create user profile object
+      // Cria array com info do user
       const userProfile = {
         uid: user.uid,
         email: user.email,
         firstName,
         lastName,
-        // Add other profile information as needed
       };
 
-      // Update existing user information
-      localStorage.setItem("user", JSON.stringify(userProfile));
-
-      // Load existing user profiles or create an empty array
+      // Carrega o user no bd
       const existingUserProfiles =
         JSON.parse(localStorage.getItem("userProfiles")) || [];
 
-      // Append userProfile to the existing array
+      // Se o array já estiver criado, só envia
       existingUserProfiles.push(userProfile);
 
-      // Save userProfiles to localStorage (persistent)
+      //  Salva no localStorage
       localStorage.setItem(
         "userProfiles",
         JSON.stringify(existingUserProfiles)
       );
 
-      // Save user accessToken to localStorage (temporary, will be cleared on logout)
-      localStorage.setItem("token", user.accessToken);
+      alert("Usuario Criado!");
 
-      // Redirect to the home page
-      navigate("/");
+      resetForm();
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleTooglePassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
   return (
-    <div>
-      <h1>Signup Page</h1>
-      <form onSubmit={handleSubmit} className="signup-form">
-        <label>
-          First Name:
-          <input
-            type="text"
-            placeholder="Your First Name"
-            required
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </label>
-        <label>
-          Last Name:
-          <input
-            type="text"
-            placeholder="Your Last Name"
-            required
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            placeholder="Your Email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            placeholder="Your Password"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <button type="submit" className="signup-button">
-          Signup
-        </button>
-      </form>
-      <p>
-        Need to Login? <Link to="/login">Login</Link>
-      </p>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        overflow: "hidden",
+        margin: "0",
+        padding: "0",
+      }}
+    >
+      <div style={{ flex: "0 0 auto", width: "40%", height: "100%" }}>
+        <img
+          src={imgCadastro}
+          alt="Imagem de Cadastro"
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+      </div>
+
+      <div
+        style={{
+          flex: "1",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "20px",
+          overflow: "hidden",
+          margin: "0",
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Cadastre-se
+        </Typography>
+        <form
+          onSubmit={handleSubmit}
+          className="signup-form"
+          style={{
+            width: "60%",
+            maxWidth: "517px",
+            height: "auto",
+            overflow: "hidden",
+            margin: "0",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              width: "100%",
+            }}
+          >
+            <TextField
+              label="Nome"
+              type="text"
+              variant="outlined"
+              margin="normal"
+              required
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              style={{ width: "50%", marginRight: "8px", marginTop: "0px" }}
+            />
+            <TextField
+              label="Sobrenome"
+              type="text"
+              variant="outlined"
+              margin="normal"
+              required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              style={{ width: "50%", marginTop: "0px" }}
+            />
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              marginBottom: "8px",
+            }}
+          >
+            <TextField
+              label="Email address"
+              type="email"
+              variant="outlined"
+              margin="normal"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={{ marginBottom: "8px", marginTop: "0px" }}
+            />
+            <TextField
+              label="Password"
+              type={showPassword ? "text" : "password"}
+              variant="outlined"
+              margin="normal"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleTooglePassword} edge="end">
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              style={{ marginTop: "0px" }}
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Cadastrar
+            </Button>
+          </div>
+        </form>
+
+        <Typography>
+          Já possui cadastro? <Link to="/login">Login</Link>
+        </Typography>
+      </div>
     </div>
   );
 };
