9c5d233547fee8a1049caa7d48f2478743471f4a
Aryel Anne Louise de Souza Gabriel <aannelouise@outlook.com>
2024-01-30 17:43:52 -0300

    Modal de preview do projeto

--- src/components/ModalPreview/ModalPreview.jsx
+++ src/components/ModalPreview/ModalPreview.jsx
@@ -1,28 +1,25 @@
 import React from "react";
 
+import Menu from '../Menu/Menu'
 import { Modal, Box, IconButton, Chip } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 import styles from "./modalpreview.module.css";
+import { ArrowBack } from "@mui/icons-material";
 
 const ModalPreview = ({ handleClose, card }) => {
   return (
     <>
-      <Modal
-        open={true}
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
+      <main className={styles.backgroundModal}>
         <section className={styles.modalPreview}>
           <Box
             sx={{
-              width: "100%",
+              width: "90%",
               display: "flex",
-              justifyContent: "flex-end",
+              justifyContent: window.innerWidth > 850 ? "flex-end" : "flex-start",
               alignItems: "flex-end",
+              marginLeft: window.innerWidth > 850 ? 0 : '20px'
             }}
           >
             <IconButton
@@ -31,65 +28,55 @@ const ModalPreview = ({ handleClose, card }) => {
               onClick={handleClose}
               aria-label="close"
             >
-              <CloseIcon sx={{ color: "#323232" }} />
+              {window.innerWidth > 850 ? <CloseIcon sx={{ color: "#323232" }} /> :
+              <ArrowBack sx={{ color: "#323232" }} />}
             </IconButton>
           </Box>
           <div className={styles.modalContent}>
-            <div className={styles.HorizontalContainer}>
-              <div className={styles.informacoes}>
-                <img
-                  src={
-                    typeof card?.avatar === "object"
-                      ? URL.createObjectURL(card?.avatar)
-                      : card?.avatar
-                  }
-                  alt="Foto de perfil"
-                />
-                <p>
-                  {card?.name} {card?.lastName}
-                  <br /> {card?.createdAt}
-                </p>
-              </div>
+            <h3>{card?.title}</h3>
+            <div className={styles.modalContentInfoImg}>
+              <div className={styles.HorizontalContainer}>
+                <div className={styles.informacoes}>
+                  <img
+                    src={
+                      typeof card?.avatar === "object"
+                        ? URL.createObjectURL(card?.avatar)
+                        : card?.avatar
+                    }
+                    alt="Foto de perfil"
+                  />
+                  <p>
+                    <b>{card?.name} {card?.lastName}</b>
+                    {card?.createdAt}
+                  </p>
+                </div>
 
-              <h3>{card?.title}</h3>
+                <h3>{card?.title}</h3>
 
-              <div className={styles.tags}>
-                {card?.tags.map((tag, index) => (
-                  <Chip label={tag} key={index} />
-                ))}
-              </div>
-            </div>
-            <div className={styles.projeto}>
-              <img
-                src={
-                  typeof card?.projectImage === "object"
-                    ? URL.createObjectURL(card?.projectImage)
-                    : card?.projectImage
-                }
-                alt={`Projeto ${card?.title}`}
-                sizes="100"
-              />
-
-              {/* mobile */}
-              <div className={styles.informacoesResponsive}>
-                <img src={card?.avatar} alt="Foto de perfil" />
-                <p>{`${card?.name} ${card?.lastName} • ${card?.createdAt}`}</p>
-
-                <div className={styles.tagsResponsive}>
+                <div className={styles.tags}>
                   {card?.tags.map((tag, index) => (
                     <Chip label={tag} key={index} />
                   ))}
                 </div>
               </div>
-              {/* ====== */}
-
+                <img
+                  src={
+                    typeof card?.projectImage === "object"
+                      ? URL.createObjectURL(card?.projectImage)
+                      : card?.projectImage
+                  }
+                  alt={`Projeto ${card?.title}`}
+                  sizes="100"
+                />
+            </div>
+            <div className={styles.projeto}>
               <p>{card?.description}</p>
               <p>Download</p>
               <a href={card?.urlGithub}>{card?.urlGithub}</a>
             </div>
           </div>
         </section>
-      </Modal>
+      </main>
     </>
   );
 };
